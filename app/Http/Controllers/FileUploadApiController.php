007db2dc85af5a92741bafdde1dccdc210d62555
StalvinAgile <stalvin.m@agileidc.com>
2022-06-21 12:51:08 +0530

    updated api for file upload

--- app/Http/Controllers/FileUploadApiController.php
+++ app/Http/Controllers/FileUploadApiController.php
@@ -2,36 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use App\Models\FileUpload;
+use Illuminate\Http\Request;
 use Illuminate\Support\Str;
-use Image;
-use Log;
 
 class FileUploadApiController extends Controller
 {
 
     public function store(Request $request)
     {
-        $request->validate([
-            'file_name' => 'file|mimes:csv,xlsx,xls|max:10000',
-        ]);
-        $image = new FileUpload();
-        $image->file_name = $this->uploadFile($request);
-        $image->save();
-        return response()->json(['status' => 'S', 'message', 'file uploaded successfully']);
+        try {
+            $request->validate([
+                'file_name' => 'file|mimes:csv,xlsx,xls|max:10000',
+            ]);
+            $file = new FileUpload();
+            $file->file_name = $this->uploadFile($request);
+            $file->save();
+            return response()->json(['status' => 'S', 'message' => 'file uploaded successfully']);
+        } catch (Exception $file_exception) {
+            return response()->json(['status' => 'E', 'message' => 'invalid file']);
+        }
     }
     protected function uploadFile(Request $request)
     {
-        if ($request->hasFile('file_name')) {
-            $image = $request->file('file_name');
-            $originalFileName = $image->getClientOriginalName();
-            $extension = $image->getClientOriginalExtension();
-            $fileNameOnly = pathinfo($originalFileName, PATHINFO_FILENAME);
-            $fileName = Str::slug($fileNameOnly) . '-' . time() . '.' . $extension;
-            return $image->storeAs('public', $fileName);
+        try {
+            if ($request->hasFile('file_name')) {
+                $file = $request->file('file_name');
+                $originalFileName = $file->getClientOriginalName();
+                $extension = $file->getClientOriginalExtension();
+                $fileNameOnly = pathinfo($originalFileName, PATHINFO_FILENAME);
+                $fileName = Str::slug($fileNameOnly) . '-' . time() . '.' . $extension;
+                return $file->storeAs('public', $fileName);
+            }
+            return null;
+        } catch (Exception $file_exception) {
+            return response()->json(['status' => 'E', 'message' => 'Error Uploading file ']);
         }
-        return null;
     }
 }
